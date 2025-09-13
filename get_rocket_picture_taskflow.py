e60109af39d2f659507aba4ffe0db52f89d37b9f
Najeeb <sulaimannajeebadesoji@gmail.com>
2025-09-13 03:38:08 +0100

    Created the DAG using task flow api

--- get_rocket_picture_taskflow.py
+++ get_rocket_picture_taskflow.py
@@ -1,6 +1,5 @@
 from airflow.sdk import DAG, task
 from airflow.providers.standard.operators.bash import BashOperator
-from airflow.providers.standard.operators.python import PythonOperator
 import pendulum
 import requests
 import json
