4976cd428dd1b5531bdaefb48c69bd3dbdcb3595
Najeeb <sulaimannajeebadesoji@gmail.com>
2025-09-18 01:25:55 +0100

    Added a DAG for complex task dependencies

---
+++ dependencies_1.py
@@ -0,0 +1,18 @@
+from airflow.sdk import DAG
+from airflow.providers.standard.operators.empty import EmptyOperator
+import pendulum
+
+
+with DAG(
+    dag_id="rocket",
+    start_date=pendulum.datetime(2025, 9, 10),
+    schedule=None,
+    catchup=False
+) as dag:
+    
+    # Linear and Parallel dependecies
+    fetch_customer = EmptyOperator(task_id="fetch_customer")
+    fetch_complaint = EmptyOperator(task_id="fetch_complaint")
+
+    clean_customer = EmptyOperator(task_id="clean_customer")
+    clean_complaint = EmptyOperator(task_id="clean_complaint")
