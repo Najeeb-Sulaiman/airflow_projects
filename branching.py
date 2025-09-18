c733f6392e6c1cbb56064fc320bfdc47e1b8e473
Najeeb <sulaimannajeebadesoji@gmail.com>
2025-09-18 03:32:26 +0100

    Added a DAG for Branching

--- branching.py
+++ branching.py
@@ -38,6 +38,7 @@ with DAG(
     fetch_complaint = EmptyOperator(task_id="fetch_complaint")
     clean_complaint = EmptyOperator(task_id="clean_complaint")
 
+    # These will not run due to lack of trigger rule
     join_datasets = EmptyOperator(task_id="join_datasets")
     train_model = EmptyOperator(task_id="train_model")
     deploy_model = EmptyOperator(task_id="deploy_model")
@@ -49,3 +50,15 @@ with DAG(
     fetch_complaint >> clean_complaint
     [clean_customer_old, clean_customer_new, clean_complaint] >> join_datasets
     join_datasets >> train_model >> deploy_model
+
+    # Dummy task to explicitly join the different branches before continuing
+    # join_crm_branch = EmptyOperator(task_id="join_crm_branch")
+
+    # start >> [pick_crm_system, fetch_complaint]
+    # pick_crm_system >> [fetch_customer_old, fetch_customer_new]
+    # fetch_customer_old >> clean_customer_old
+    # fetch_customer_new >> clean_customer_old
+    # [clean_customer_old, clean_customer_new] >> join_crm_branch
+    # fetch_complaint >> clean_complaint
+    # [join_crm_branch, clean_complaint] >> join_datasets
+    # join_datasets >> train_model >> deploy_model
